2e8573dce2eaa431a88c425a33b898c4c9f9d292
Ubuntu <ubuntu@ip-172-31-39-225.us-east-2.compute.internal>
2019-03-21 22:08:05 +0000

    creating home list jobs

--- js/home.js
+++ js/home.js
@@ -18,18 +18,37 @@ function getTopJobOffers(){
 			}
 	});
 }
-function searchJobOffers(keyword,city){
-	var url = "http://18.221.163.161:8080/ws/getAllOffers",
+
+function searchJobOffers2(keyword,city){
+	var urlPost = "http://18.221.163.161:8080/ws/getAllOffers",
 	str = {"filter_keyword":keyword,"filter_city":city};
-	$.post(url,$.param(str), function(data){
-		console.log(data);
-		alert(data);
-	}).fail(function(data){
-		console.log(data);
-		alert(data);
+	var dataPost =$.param(str); 
+	$.ajax({
+			type: "POST",
+			url: urlPost,
+			data: dataPost,
+			contentType: "application/x-www-form-urlencoded",
+			dataType: "json",
+			success: function (data) {
+				topJobs(data.message);
+			},
+			failure: function (data) {
+				$(".job-listings-sec").html("<h3>There are no job offers available.</h3>")
+			}
+	});
+}
+function topJobs(data){
+	$.each(data,function(i,d){
+		var html = "<a href='" + d.path_image_company + "' class='job-listing rounded'>";
+			html += "<div class='job-title-sec'><div class='c-logo'><img src='"+d.path_image_company+"' alt='' /> </div><h3>"+d.nameCompany+"</h3><span>"+d.position+"</span></div>";
+			html += "<span class='job-lctn'><i class='la la-map-marker'></i>"+d.city+"</span>";
+			html += "<span class='job-dates'><i class='la la-calendar-o'></i>"+d.date_init+" - "+d.date_end+"</span>";
+			html += "<span class='job-is'>"+d.reward+" €</span>";
+			html += "</a>";
+		$(".job-listings-sec").append(html);
 	});
 }
 
 $(document).ready(function(){
-	searchJobOffers("","");
+	searchJobOffers2("","");
 });
